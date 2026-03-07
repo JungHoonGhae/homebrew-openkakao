62271582ed048e191ddd2a044aec7fb34f7f2dad
Lucas <42439321+JungHoonGhae@users.noreply.github.com>
2026-03-08 02:06:35 +0900

    openkakao-rs 0.4.1

--- Formula/openkakao-rs.rb
+++ Formula/openkakao-rs.rb
@@ -1,8 +1,8 @@
 class OpenkakaoRs < Formula
   desc "KakaoTalk CLI client with REST API and LOCO protocol support"
   homepage "https://github.com/JungHoonGhae/openkakao"
-  url "https://github.com/JungHoonGhae/openkakao/archive/refs/tags/v0.4.0.tar.gz"
-  sha256 "1870566ad112b9e5f3b5277ecdaf0e186e179a563ec00c3d2b699a579216a3b7"
+  url "https://github.com/JungHoonGhae/openkakao/archive/refs/tags/v0.4.1.tar.gz"
+  sha256 "14e1e32113dcc3120899744dc1e59f88bd4babdf4bbb5341e961d97643728e49"
   license "MIT"
 
   depends_on "rust" => :build
@@ -14,6 +14,9 @@ class OpenkakaoRs < Formula
   end
 
   test do
-    assert_match "openkakao-rs", shell_output("#{bin}/openkakao-rs --help")
+    assert_match version.to_s, shell_output("#{bin}/openkakao-rs --version")
+    send_help = shell_output("#{bin}/openkakao-rs send --help")
+    assert_match "--no-prefix", send_help
+    assert_match "-y, --yes", send_help
   end
 end
